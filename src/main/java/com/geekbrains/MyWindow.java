42e379d9591c02531227e7ebbc3bb7cc4b442bef
South <south123@mail.ru>
2022-01-17 12:47:05 +0300

    Lesson-8

--- src/main/java/com/geekbrains/MyWindow.java
+++ src/main/java/com/geekbrains/MyWindow.java
@@ -2,13 +2,16 @@ package com.geekbrains;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class MyWindow extends JFrame {
+    private boolean textNeedStart = false;
+    private Calculator calculator = new Calculator();
 
     public MyWindow() {
 
-        Calculator calculator = new Calculator();
 
         setTitle("Calculator");
         setBounds(2000, 300, 400, 400);
@@ -16,6 +19,16 @@ public class MyWindow extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JTextField field = new JTextField();
         add(field, BorderLayout.NORTH);
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculator.reset();
+                field.setText("");
+                textNeedStart=true;
+            }
+        });
+        add(clearButton, BorderLayout.SOUTH);
 
 
         JPanel panel = new JPanel(new GridLayout(4, 4));
@@ -24,22 +37,123 @@ public class MyWindow extends JFrame {
 
         JButton[] buttons = new JButton[16];
 
-        buttons[0]=new JButton("1");
-        buttons[1]=new JButton("2");
-        buttons[2]=new JButton("3");
-        buttons[3]=new JButton("/");
-        buttons[4]=new JButton("4");
-        buttons[5]=new JButton("5");
-        buttons[6]=new JButton("6");
-        buttons[7]=new JButton("*");
-        buttons[8]=new JButton("7");
-        buttons[9]=new JButton("8");
-        buttons[10]=new JButton("9");
-        buttons[11]=new JButton("+");
-        buttons[12]=new JButton("0");
-        buttons[13]=new JButton(".");
-        buttons[14]=new JButton("=");
-        buttons[15]=new JButton("-");
+        buttons[0] = new JButton("1");
+        buttons[0].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "1");
+            }
+        });
+        buttons[1] = new JButton("2");
+        buttons[1].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "2");
+            }
+        });
+        buttons[2] = new JButton("3");
+        buttons[2].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "3");
+            }
+        });
+        buttons[3] = new JButton("/");
+        buttons[3].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operationCalculate(field, Calculator.Operation.DIVIDE);
+            }
+        });
+        buttons[4] = new JButton("4");
+        buttons[4].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "4");
+            }
+        });
+        buttons[5] = new JButton("5");
+        buttons[5].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "5");
+            }
+        });
+        buttons[6] = new JButton("6");
+        buttons[6].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "6");
+            }
+        });
+        buttons[7] = new JButton("*");
+        buttons[7].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operationCalculate(field, Calculator.Operation.MULTIPLY);
+            }
+        });
+        buttons[8] = new JButton("7");
+        buttons[8].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "7");
+            }
+        });
+        buttons[9] = new JButton("8");
+        buttons[9].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "8");
+            }
+        });
+        buttons[10] = new JButton("9");
+        buttons[10].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "9");
+            }
+        });
+        buttons[11] = new JButton("+");
+        buttons[11].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operationCalculate(field, Calculator.Operation.SUM);
+            }
+        });
+        buttons[12] = new JButton("0");
+        buttons[12].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, "0");
+            }
+        });
+        buttons[13] = new JButton(".");
+        buttons[13].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printToField(field, ".");
+            }
+        });
+        buttons[14] = new JButton("=");
+        buttons[14].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (calculator.setOperand(Float.valueOf(field.getText()))) {
+                    field.setText(String.valueOf(calculator.result()));
+                    textNeedStart = true;
+                } else
+                    JOptionPane.showMessageDialog(null, "Необходимо нажать =");
+                calculator.reset();
+            }
+        });
+        buttons[15] = new JButton("-");
+        buttons[15].addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operationCalculate(field, Calculator.Operation.MINUS);
+            }
+        });
 
 
         for (JButton button : buttons) {
@@ -47,6 +161,23 @@ public class MyWindow extends JFrame {
         }
 
         setVisible(true);
+    }
+
+    private void printToField(JTextField field, String simbol) {
+        if (textNeedStart) {
+            field.setText(simbol);
+            textNeedStart=false;
+        }
+        else
+            field.setText(field.getText() + simbol);
+    }
+
+    private void operationCalculate(JTextField field, Calculator.Operation multiply) {
+        if (calculator.setOperand(Float.valueOf(field.getText()))) {
+            calculator.setOperation(multiply);
+            textNeedStart = true;
+        } else
+            JOptionPane.showMessageDialog(null, "Необходимо нажать =");
     }
 
     public static void main(String[] args) {
